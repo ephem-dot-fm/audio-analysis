88a2ecdbb17c05c739aa746fccc81406a486f0c9
Max McCready <msmccready@gmail.com>
2022-09-05 18:24:40 -0400

    vacant

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from yellow import run_threaded
 import time
 
 if __name__ == "__main__":
-    file_duration = 20
-    schedule.every(file_duration).seconds.do(
-        run_threaded, station_name='SOMACLQ', file_duration=file_duration)
+    print('aloha')
+    # file_duration = 20
+    # schedule.every(file_duration).seconds.do(
+    #     run_threaded, station_name='SOMACLQ', file_duration=file_duration)
 
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    # while True:
+    #     schedule.run_pending()
+    #     time.sleep(1)
