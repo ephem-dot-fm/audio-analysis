8e8e07b20c8f741a0ffc1d8248abbab6ce4428c6
Max McCready <msmccready@gmail.com>
2022-09-05 18:03:13 -0400

    added main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import schedule
+from yellow import run_threaded
+import time
+
+if __name__ == "__main__":
+    file_duration = 20
+    schedule.every(file_duration).seconds.do(
+        run_threaded, station_name='SOMACLQ', file_duration=file_duration)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
