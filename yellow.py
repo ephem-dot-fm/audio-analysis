85f5bec1311cc135201d29b277c4bc481f4998c0
Max McCready <msmccready@gmail.com>
2022-09-05 18:12:13 -0400

    neautralized

--- yellow.py
+++ yellow.py
@@ -14,10 +14,11 @@ def run_threaded(station_name, file_duration):
 
 # every 20 seconds, begins writing + analyzing a new stream
 if __name__ == "__main__":
-    file_duration = 20
-    schedule.every(file_duration).seconds.do(
-        run_threaded, station_name='SOMACLQ', file_duration=file_duration)
+    print("hi there!")
+    # file_duration = 20
+    # schedule.every(file_duration).seconds.do(
+    #     run_threaded, station_name='SOMACLQ', file_duration=file_duration)
 
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    # while True:
+    #     schedule.run_pending()
+    #     time.sleep(1)
