986e1b815d47530d74c9ab28f15d5a25300963ab
Max McCready <msmccready@gmail.com>
2022-09-05 17:06:11 -0400

    hola

---
+++ yellow.py
@@ -0,0 +1,23 @@
+### COMMAND CENTER FOR RETRIEVAL ###
+import time
+from yellows_radar import write_stream
+
+import threading
+import schedule
+
+
+def run_threaded(station_name, file_duration):
+    job_thread = threading.Thread(
+        target=write_stream, args=(station_name, file_duration))
+    job_thread.start()
+
+
+# every 20 seconds, begins writing + analyzing a new stream
+if __name__ == "__main__":
+    file_duration = 20
+    schedule.every(file_duration).seconds.do(
+        run_threaded, station_name='SOMACLQ', file_duration=file_duration)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
---
+++ yellows_radar.py
@@ -0,0 +1,69 @@
+
+import multiprocessing
+import requests
+import time
+import os
+from pydub import AudioSegment
+import subprocess
+import os
+from collections import deque
+
+
+stations = {
+    'DDR': 'https://dublindigitalradio.out.airtime.pro/dublindigitalradio_a',
+    'KUTX': 'https://kut.streamguys1.com/kutx-web',
+    'WPRB': 'https://wprb.streamguys1.com/live',
+    'SOMADSO': 'https://ice6.somafm.com/deepspaceone-128-mp3',
+    'SOMADESI': 'https://ice1.somafm.com/suburbsofgoa-128-mp3',
+    'SOMAMTL': 'https://ice1.somafm.com/metal-128-mp3',
+    'SOMACLQ': 'https://ice6.somafm.com/cliqhop-128-mp3',
+    'SOMAFOLK': 'https://ice4.somafm.com/folkfwd-128-mp3',
+    'SOMALUSH': 'https://ice1.somafm.com/lush-128-mp3',
+    'SOMAPOPT': 'https://ice1.somafm.com/poptron-128-mp3',
+    'SOMAVPR': 'https://ice2.somafm.com/vaporwaves-128-mp3',
+    'SOMAIND': 'https://ice4.somafm.com/indiepop-128-mp3'
+}
+
+
+def stream_to_mp3(station_url, file_name):
+    with requests.get(station_url, stream=True) as r:
+        with open(f'{file_name}.mp3', 'wb') as f:
+            try:
+                for block in r.iter_content(1024):
+                    f.write(block)
+            except KeyboardInterrupt:
+                f.close()
+
+
+def mp3_to_wav(file):
+    src = f'{file}.mp3'
+    dst = f'{file}.wav'
+
+    try:
+        audSeg = AudioSegment.from_file(src, "mp3")
+        audSeg.export(dst, format="wav")
+    except FileNotFoundError as f:
+        print("Couldn't find that file homie")
+    except:
+        audSeg = AudioSegment.from_file(src, format="mp4")
+        audSeg.export(dst, format="wav")
+
+
+def write_stream(station_name, file_duration):
+    file_name = f'soundbytes/{station_name}_{str(int(time.time()))}'
+
+    # create a new Process which allows me to write a stream for x amount of time
+    p = multiprocessing.Process(
+        target=stream_to_mp3, name="Write Stream to MP3", args=(stations[station_name], file_name))
+
+    p.start()
+    time.sleep(file_duration)
+    p.terminate()
+
+    # convert mp3 to WAV file
+    mp3_to_wav(file_name)  # converting to WAV file
+    wav_file_name = f'{file_name}.wav'
+
+
+if __name__ == "__main__":
+    pass
