db0f498f00dcb6b7d6fe20be3de647d51c0bcd9c
Max McCready <msmccready@gmail.com>
2022-09-07 14:47:23 -0400

    get cwd

---
+++ red.py
@@ -0,0 +1,12 @@
+# print out all the files in a given directory
+
+# find the files that need to be deleted
+# write the script to delete them
+#
+
+import os
+
+
+def rid_soundfiles():
+    cwd = os.getcwd()
+    print('CWD: ', cwd)
