49d7dfcf480784600873208bdb1607d73e04ec9d
Max McCready <msmccready@gmail.com>
2022-09-06 23:21:36 -0400

    changed variable names in hue function

--- hue.py
+++ hue.py
@@ -246,12 +246,12 @@ def _to_sliding_window(data, window_length, hop_length):
     return np.concatenate(windowed_data, axis=0)
 
 
-def get_hue(model, file_path):
+def get_hue(model_name, file_path):
     # initialize the model (may be re-used for multiple files)
     classifier = TempoClassifier(model_name)
 
     # read the file's features
-    features = read_features(input_file)
+    features = read_features(file_path)
 
     # estimate the global tempo
     tempo = classifier.estimate_tempo(features, interpolate=False)
