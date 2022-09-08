b7bc8748deac7e02a079f86ab59512674e8d79ea
Max McCready <msmccready@gmail.com>
2022-09-08 15:25:18 -0400

    reducing size

--- hsl.py
+++ hsl.py
@@ -1,9 +1,9 @@
 from webbrowser import get
 from hue import hue
-import pyloudnorm as pyln
+# import pyloudnorm as pyln
 import librosa
-import numpy as np
-import soundfile as sf
+# import numpy as np
+# import soundfile as sf
 
 
 # time-based characteristics => base color
@@ -14,28 +14,28 @@ def hue(file_name):
     return hue
 
 
-# how saturated color is => loudness
-def saturation(file):
-    # load audio (with shape (samples, channels))
-    data, rate = sf.read(file)
-    meter = pyln.Meter(rate)  # create BS.1770 meter
-    loudness = meter.integrated_loudness(data)  # measure loudness
+# # how saturated color is => loudness
+# def saturation(file):
+#     # load audio (with shape (samples, channels))
+#     data, rate = sf.read(file)
+#     meter = pyln.Meter(rate)  # create BS.1770 meter
+#     loudness = meter.integrated_loudness(data)  # measure loudness
 
-    return loudness
+#     return loudness
 
 
-# lightness to darkness => spectral centroid which corresponds to 'brightness' of tone
-def value(file):
-    y, sr = librosa.load(file, sr=None)
+# # lightness to darkness => spectral centroid which corresponds to 'brightness' of tone
+# def value(file):
+#     y, sr = librosa.load(file, sr=None)
 
-    # get spectral centroid, mean of that, and masked mean using librosa
-    centroid = librosa.feature.spectral_centroid(y=y, sr=sr)
-    centroid_arr = centroid[0]
-    regular_mean = centroid_arr.mean()
-    masked = np.ma.masked_equal(centroid, 0)
-    masked_mean = masked.mean()
+#     # get spectral centroid, mean of that, and masked mean using librosa
+#     centroid = librosa.feature.spectral_centroid(y=y, sr=sr)
+#     centroid_arr = centroid[0]
+#     regular_mean = centroid_arr.mean()
+#     masked = np.ma.masked_equal(centroid, 0)
+#     masked_mean = masked.mean()
 
-    return regular_mean
+#     return regular_mean
 
 
 def hsl(file_name):
